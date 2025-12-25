208e90f3302215f81214dae605851baa742840a7
raut-abhishek <iamabhishekraut01@gmail.com>
2025-12-25 22:57:04 +0530

    Vite server setup and devtool setup

--- electron/main.js
+++ electron/main.js
@@ -1,15 +1,24 @@
-// @ts-check
-
 const { app, BrowserWindow } = require("electron");
-
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
-  win.loadFile("index.html");
+
+  win.loadURL("http://localhost:5173");
+
+
+  win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
